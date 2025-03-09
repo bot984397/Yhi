f1d93e278068d62e4d62e41f842bea9c3749fc92
nightcall489 <nightcall489@gmail.com>
2025-03-09 16:12:25 +0100

    init

---
+++ BIOS.asm
@@ -0,0 +1,14 @@
+   ;------------------------------------------------;               
+   ;  SECURE IBM 5150 BIOS IMPLEMENTATION. ANY AND  ;                
+   ;   ALL BIOS ROUTINES ARE MEANT TO BE ACCESSED   ;                 
+   ;       THROUGH SOFTWARE INTERRUPTS ONLY.        ;                      
+   ;------------------------------------------------; 
+
+%MACRO PROC 1
+%1:
+%ENDMACRO
+
+   ;------------------------------------------------;               
+   ;             POWER ON RESET VECTOR              ;                            
+   ;------------------------------------------------;  
+
